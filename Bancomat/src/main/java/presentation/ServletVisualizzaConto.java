3458d56b73c08364cd4bc95a8ef5cd1ca4c32dd4
AdrianoBoriosi <102974230+AdrianoBoriosi@users.noreply.github.com>
2022-07-21 21:15:09 +0200

    Add files via upload

---
+++ Bancomat/src/main/java/presentation/ServletVisualizzaConto.java
@@ -0,0 +1,47 @@
+package presentation;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import business.ContoCorrenteEJB;
+import data.ContoCorrente;
+
+public class ServletVisualizzaConto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@EJB
+	ContoCorrenteEJB contoSrv;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletVisualizzaConto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ContoCorrente conto=new ContoCorrente();
+		String operazione="saldo";
+		Float  quantita=(float)0;
+		//PrintWriter out=response.getWriter();
+		Integer numConto= Integer.valueOf(request.getParameter("numConto"));
+		Integer pinConto= Integer.valueOf(request.getParameter("pinConto"));
+		HttpSession session=request.getSession();
+		
+		if (contoSrv.controllaOperazione(operazione, quantita, pinConto, numConto)) {
+		conto=contoSrv.getContoCorrente(pinConto,numConto);
+		System.out.println(conto.getInstestatario());
+		session.setAttribute("Conto",conto);
+		session.setAttribute("Operazione",operazione);
+		session.setAttribute("Quantita",quantita);
+		request.getServletContext().getRequestDispatcher("/conto.jsp").forward(request, response);
+	}
+	}
+}
